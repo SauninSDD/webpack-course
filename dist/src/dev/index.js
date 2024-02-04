8491f327249606fa2178f8d15f57aefd39b82d8d
SauninSDD <132187522+SauninSDD@users.noreply.github.com>
2024-02-04 12:40:08 +0300

    Deleted directory dict

--- dist/src/dev/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { useInitial } from "./useInitial";
-var ComponentPreviews = React.lazy(function () { return import("./previews"); });
-export { ComponentPreviews, useInitial };
